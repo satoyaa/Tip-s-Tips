8c5cbd0169d9f7f9da6337b4fca2a494aa075a5f
SURFACE\satom <h20025@e.hiroshima-cu.ac.jp>
2026-03-19 20:00:07 +0900

    style comment out

--- tips-tips/src/Components/Tip.jsx
+++ tips-tips/src/Components/Tip.jsx
@@ -26,6 +26,7 @@ const Tip = ({data, setTips}) => {
 
     return(
         <div className="tip" style={{top:`${data.tipTop}px`, left:`${data.tipLeft}px`, transform: `rotate(${data.tipRotate}deg)`}}>
+            {/*<div className="tip" style={{top:`${data.tipTop}px`, left:`${data.tipLeft}px`, transform: `rotate(${data.tipRotate}deg)`, background: `#{${data.color}}`}}>*/}
             <a href={data.tipDetails} className="tipDetails" aria-label="詳細を見る" />
             <h2 className="tipTitle">{data.tipTitle}</h2>
             <p className="tipText">{data.tipExplanation}</p>
